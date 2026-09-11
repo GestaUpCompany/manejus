0e8bc6efcaea40181b3106b42edef2c02d494d46
Victor Hugo Santos de Moraes <victormoraes065@gmail.com>
2026-09-11 13:40:54 -0400

    fix: simplifica vercel.json e remove function de teste

--- api/pdf/teste.ts
+++
@@ -1,8 +0,0 @@
-type Response = {
-  status: (code: number) => Response
-  json: (body: unknown) => void
-}
-
-export default async function handler(_req: unknown, res: Response) {
-  return res.status(200).json({ ok: true, message: 'Function de teste operacional' })
-}
